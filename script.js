4d077319bbdfdcbc73f781203e89abc84e6e8e93
leonhossain <leon.hossain2427@gmail.com>
2018-10-04 03:24:35 +0000

    master

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 /* JavaScript code here */
+console.log("External");
+
+function sumArray(arr)
+{
+    var sum = 0;
+    
+    arr.forEach(function(value, index)
+    {
+        sum = sum + value;
+    });
+}
+
+console.log(sumArray([1,2,3,4,5,6]));
